2ef24e8f0f6077ab95881da01bbaf81f04c2f08f
ryutlis <yalewhd@gmail.com>
2019-12-01 14:58:24 +0000

    init

---
+++ code/aoc_1.h
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+
+namespace aoc_1 {
+    int64_t run() {
+        return 0;
+    }
+} // namespace aoc_1
---
+++ code/aoc_runner.cpp
@@ -0,0 +1,7 @@
+#include "./aoc_1.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Day 1: " << aoc_1::run() << "\n";
+
+}
