71cf32e2ee6542f943f2c6b5628e453b333c2170
ak1r4 <ak1r4.j@gmail.com>
2019-12-05 07:17:06 +0000

    Day 4

--- code/aoc_4.h
+++ code/aoc_4.h
@@ -5,11 +5,54 @@
 #include <cstdint>
 
 namespace aoc_4 {
+    bool check_number(std::string const & number, size_t expected_count) {
+        char prev = '0';
+        size_t count = 1;
+        size_t highest_count = 0;
+
+        for (auto const & c: number) {
+            if (c < prev) {
+                return false;
+            }
+
+            if (c == prev) {
+                ++count;
+            } else {
+                highest_count = std::max(count, highest_count);
+                count = 1;
+            }
+            prev = c;
+        }
+
+        highest_count = std::max(count, highest_count);
+
+        if (expected_count == 0) {
+            return highest_count >= 2;
+        } else {
+            return highest_count == expected_count;
+        }
+    }
+
     int64_t run_1(int64_t range_start, int64_t range_end) {
-        return 0;
+        size_t count = 0;
+        for (int64_t n = range_start; n <= range_end; n++) {
+            std::string str_n = std::to_string(n);
+            if (check_number(str_n, 0)) {
+                count++;
+            }
+        }
+        return count;
+
     }
     int64_t run_2(int64_t range_start, int64_t range_end) {
-        return 0;
+        size_t count = 0;
+        for (int64_t n = range_start; n <= range_end; n++) {
+            std::string str_n = std::to_string(n);
+            if (check_number(str_n, 2)) {
+                count++;
+            }
+        }
+        return count;
     }
 
 } // namespace aoc_4
